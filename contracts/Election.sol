b46ce0f9ae1e8ee24d2eaefdb312fa065a757808
Sharma-Hrishabh <30hrs1999@gmail.com>
2019-01-07 19:12:12 +0530

    first-one

---
+++ contracts/Election.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.4.22;
+
+
+contract Election {
+  string public candidate;
+  constructor() public {
+    candidate="Candidate 1";
+  }
+}
---
+++ migrations/1546867657_election.js
@@ -0,0 +1,6 @@
+var Election = artifacts.require('./Election.sol');
+
+module.exports = function(deployer) {
+  // Use deployer to state migration tasks.
+  deployer.deploy(Election);
+};
