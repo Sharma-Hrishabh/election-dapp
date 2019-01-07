51d9b5186ea74997c34674f06168eebc578221c2
Sharma-Hrishabh <30hrs1999@gmail.com>
2019-01-07 21:57:40 +0530

    test for correct initialisation of candidates

--- test/election.js
+++ test/election.js
@@ -8,5 +8,26 @@ contract('Election',function(accounts) {
     }).then(function(count){
         assert.equal('2',count);
     });
-});
+  });
+
+   it('initilises the candidates with correct values', () => {
+       return Election.deployed().then((instance)=>{
+        electionInstance = instance;
+        return electionInstance.candidates(1);
+       }).then((candidate)=>{
+           assert.equal(candidate[0], 1, "contains correct id");
+            assert.equal(candidate[1], "Hrishabh", "correct candidate1 name");
+            assert.equal(candidate[2],0, "correct votecount1");
+            return electionInstance.candidates(2);
+       }).then((candidate)=>{
+        assert.equal(candidate[0], 2, "contains correct id");
+        assert.equal(candidate[1], "Aman", "correct candidate2 name");
+        assert.equal(candidate[2], "0", "correct votecount2");
+       });
+       
+   });
+
+
+
+
 });
